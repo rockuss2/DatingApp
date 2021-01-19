852e6b677edb2f71bcce49890c644ce5ab252616
Armando Fernandez <afernandez@nautilosistemas.com>
2021-01-18 23:47:17 -0300

    039 Adding a login endpoint

--- API/DTO/LoginDto.cs
+++ API/DTO/LoginDto.cs
@@ -1,8 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace API.DTO
 {
-    public class RegisterDto
+    public class LoginDto
     {
         public string UserName { get; set; }
         public string Password { get; set; }
